0c465d1829354bec211e7ec7752b9cb852b92eb2
lijinlong <lijinlong@hellopics.me>
2014-10-28 17:54:47 +0800

    nothing

---
+++ src/main/java/me/duoduo/instagram/base/Instagram.java
@@ -0,0 +1,10 @@
+package me.duoduo.instagram.base;
+
+import me.duoduo.instagram.client.Media;
+
+
+
+public class Instagram extends Media {
+
+    
+}
--- src/main/java/me/duoduo/instagram/client/Media.java
+++ src/main/java/me/duoduo/instagram/client/Media.java
@@ -2,24 +2,24 @@ package me.duoduo.instagram.client;
 
 import java.util.Map;
 
-public class Media extends BaseClient {
+public class Media extends AbstractClient {
     
-    public Map<String, Object> media_search(double lat, double lng) throws Exception{
-        return media_search(lat, lng, null);
+    public Map<String, Object> mediaSearch(double lat, double lng) throws Exception{
+        return mediaSearch(lat, lng, null);
     }
     
-    public Map<String, Object> media_popular() throws Exception{
-        return media_popular(null);
+    public Map<String, Object> mediaPopular() throws Exception{
+        return mediaPopular(null);
     }
     
-    public Map<String, Object> media_search(double lat, double lng, String access_token) throws Exception {
+    public Map<String, Object> mediaSearch(double lat, double lng, String access_token) throws Exception {
         Map<String, Object> params = basic_params(access_token);
         params.put("lat", lat);
         params.put("lng", lng);
         return get("media/popular", params);
     }
 
-    public Map<String, Object> media_popular(String access_token) throws Exception {
+    public Map<String, Object> mediaPopular(String access_token) throws Exception {
         return get("media/popular", basic_params(access_token));
     }
 }
